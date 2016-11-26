4191727b672d94850fd2a11e3bfa55b3be9b02b1
GBagretsov <gbagretsov@gmail.com>
2016-11-26 14:53:46 +0300

    Добавлена библиотека VkNet, создан объект для работы с ней

---
+++ vk-sea-wf/VkApiHolder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using VkNet;
+
+namespace vk_sea_wf
+{
+    public class VkApiHolder
+    {
+        private static readonly Lazy<VkApi> vkApiHolder = new Lazy<VkApi>(() => new VkApi());
+
+        private VkApiHolder() { }
+
+        public static VkApi Api
+        {
+            get { return vkApiHolder.Value; }
+        }
+    }
+}
